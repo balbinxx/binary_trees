3ce296f6925839837f1f07736159af1c9c498fb0
balbinxx <1815@holbertonschool.com>
2021-03-11 14:01:32 -0500

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: parent node.
+ * @value: integer value.
+ * Return: new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	return (new);
+}
