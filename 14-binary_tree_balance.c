37384ce86d32b1786f18be176338ebfd9ed56d1e
balbinxx <1815@holbertonschool.com>
2021-03-12 11:01:13 -0500

    add add

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,3 +11,17 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (tree_height(tree->left) - tree_height(tree->right));
 }
+int tree_height(const binary_tree_t *tree)
+{
+	int lh = 0, rh = 0;
+
+	if (!tree)
+		return (-1);
+	if (tree->left)
+		lh = tree_height(tree->left) + 1;
+	if (tree->right)
+		rh = tree_height(tree->right) + 1;
+	if (lh >= rh)
+		return (lh);
+	return (rh);
+}
